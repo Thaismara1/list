5e63342f4071b1c660f168ccd51e14081046bb70
CEM <CEM@DESKTOP-KK3A9AD>
2023-09-20 15:30:20 +0100

    Add project files.

---
+++ list/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+using list.models;
+
+Console.WriteLine("Hello, World!");
+
+const int price_apple = 10;
+const int price_orange = 15;
+
+product p1 = new product("apple", price_apple);
+product p2 = new product("orange", price_orange);
+
+List<product> products = new List<product>();
+
+products.Add(p1);
+products.Add(p2);
+
+foreach (product currentProduct in products)
+{
+    Console.WriteLine(currentProduct.ToString());
+
+}
---
+++ list/models/product.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace list.models
+{
+    public class product
+    {
+        public int price { get; set; }
+
+        public string title { get; set; }
+
+        public product(string _title, int _price)
+        {
+            price = _price;
+            title = _title;
+        }
+
+        public override string ToString()
+        {
+            return $"{title} {price}";
+        }
+    }
+
+}
